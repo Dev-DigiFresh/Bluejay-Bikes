27b64a54239aa5a89ac3a0d5136ce280e90d18d9
Vinicius Assis <viniciusassis.dev@hotmail.com>
2022-05-26 22:49:57 -0300

    style: add video size to big devices

--- atoms/NewportVideo/NewportVideo.jsx
+++ atoms/NewportVideo/NewportVideo.jsx
@@ -2,7 +2,7 @@ import { Center } from '@chakra-ui/react';
 import YouTubePlayer from 'react-player/youtube';
 
 const NewportVideo = ({ url }) => (
-  <Center maxW="377px" maxH="188px">
+  <Center h={['188px', '320px']}>
     <YouTubePlayer url={url} controls width="100%" height="100%" />
   </Center>
 );
